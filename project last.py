99f1c5c4b702544e9b364b266f720bcf10785172
swordshik <96174438+swordshik@users.noreply.github.com>
2023-11-13 15:48:25 +0600

    Update project last.py
    simplifying defining of maps

--- project last.py
+++ project last.py
@@ -201,172 +201,89 @@ def checknear(row, column, list):
 
 #LIBRARY OF MAPS
 #first map
-linesmap00 = [0]
-linesmap00 = linesmap00*7
-linesmap01 = [0]
-linesmap01 = linesmap01*7
-linesmap02 = [0]
-linesmap02 = linesmap02*7
-linesmap03 = [0]
-linesmap03 = linesmap03*7
-linesmap04 = [0]
-linesmap04 = linesmap04*7
-linesmap05 = [0]
-linesmap05 = linesmap05*7
-linesmap06 = [0]
-linesmap06 = linesmap06*7
-map0 = [linesmap00, linesmap01, linesmap02, linesmap03, linesmap04, linesmap05, linesmap06]
-map0[0][0] = 1
-map0[0][6] = 1
-map0[1][2] = 1
-map0[1][4] = 1
-map0[2][2] = 1
-map0[2][4] = 1
-map0[4][2] = 1
-map0[4][3] = 1
-map0[4][4] = 1
-map0[6][0] = 1
-map0[6][6] = 1
-
+def map0(map0):
+    map0[0][0] = 1
+    map0[0][6] = 1
+    map0[1][2] = 1
+    map0[1][4] = 1
+    map0[2][2] = 1
+    map0[2][4] = 1
+    map0[4][2] = 1
+    map0[4][3] = 1
+    map0[4][4] = 1
+    map0[6][0] = 1
+    map0[6][6] = 1
+    return map0
 #second map
-linesmap10 = [0]
-linesmap10 = linesmap10*7
-linesmap11 = [0]
-linesmap11 = linesmap11*7
-linesmap12 = [0]
-linesmap12 = linesmap12*7
-linesmap13 = [0]
-linesmap13 = linesmap13*7
-linesmap14 = [0]
-linesmap14 = linesmap14*7
-linesmap15 = [0]
-linesmap15 = linesmap15*7
-linesmap16 = [0]
-linesmap16 = linesmap16*7
-map1 = [linesmap10, linesmap11, linesmap12, linesmap13, linesmap14, linesmap15, linesmap16]
-map1[0][1] = 1
-map1[0][5] = 1
-map1[1][1] = 1
-map1[1][5] = 1
-map1[3][0] = 1
-map1[3][2] = 1
-map1[3][4] = 1
-map1[3][6] = 1
-map1[6][2] = 1
-map1[6][3] = 1
-map1[6][4] = 1
-
+def map1(map1):
+    map1[0][1] = 1
+    map1[0][5] = 1
+    map1[1][1] = 1
+    map1[1][5] = 1
+    map1[3][0] = 1
+    map1[3][2] = 1
+    map1[3][4] = 1
+    map1[3][6] = 1
+    map1[6][2] = 1
+    map1[6][3] = 1
+    map1[6][4] = 1
+    return map1
 #third map
-linesmap20 = [0]
-linesmap20 = linesmap20*7
-linesmap21 = [0]
-linesmap21 = linesmap21*7
-linesmap22 = [0]
-linesmap22 = linesmap22*7
-linesmap23 = [0]
-linesmap23 = linesmap23*7
-linesmap24 = [0]
-linesmap24 = linesmap24*7
-linesmap25 = [0]
-linesmap25 = linesmap25*7
-linesmap26 = [0]
-linesmap26 = linesmap26*7
-map2 = [linesmap20, linesmap21, linesmap22, linesmap23, linesmap24, linesmap25, linesmap26]
-map2[0][0] = 1
-map2[1][4] = 1
-map2[2][4] = 1
-map2[2][0] = 1
-map2[3][0] = 1
-map2[2][2] = 1
-map2[4][4] = 1
-map2[5][0] = 1
-map2[5][1] = 1
-map2[5][2] = 1
-map2[6][6] = 1
-
+def map2(map2):
+    map2[0][0] = 1
+    map2[1][4] = 1
+    map2[2][4] = 1
+    map2[2][0] = 1
+    map2[3][0] = 1
+    map2[2][2] = 1
+    map2[4][4] = 1
+    map2[5][0] = 1
+    map2[5][1] = 1
+    map2[5][2] = 1
+    map2[6][6] = 1
+    return map2
 #fourth map
-linesmap30 = [0]
-linesmap30 = linesmap30*7
-linesmap31 = [0]
-linesmap31 = linesmap31*7
-linesmap32 = [0]
-linesmap32 = linesmap32*7
-linesmap33 = [0]
-linesmap33 = linesmap33*7
-linesmap34 = [0]
-linesmap34 = linesmap34*7
-linesmap35 = [0]
-linesmap35 = linesmap35*7
-linesmap36 = [0]
-linesmap36 = linesmap36*7
-map3 = [linesmap30, linesmap31, linesmap32, linesmap33, linesmap34, linesmap35, linesmap36]
-map3[0][6] = 1
-map3[1][1] = 1
-map3[2][1] = 1
-map3[2][4] = 1
-map3[2][6] = 1
-map3[3][6] = 1
-map3[4][2] = 1
-map3[5][4] = 1
-map3[5][5] = 1
-map3[5][6] = 1
-map3[6][0] = 1
-
+def map3(map3):
+    map3[0][6] = 1
+    map3[1][1] = 1
+    map3[2][1] = 1
+    map3[2][4] = 1
+    map3[2][6] = 1
+    map3[3][6] = 1
+    map3[4][2] = 1
+    map3[5][4] = 1
+    map3[5][5] = 1
+    map3[5][6] = 1
+    map3[6][0] = 1
+    return map3
 #fifth map
-linesmap40 = [0]
-linesmap40 = linesmap40*7
-linesmap41 = [0]
-linesmap41 = linesmap41*7
-linesmap42 = [0]
-linesmap42 = linesmap42*7
-linesmap43 = [0]
-linesmap43 = linesmap43*7
-linesmap44 = [0]
-linesmap44 = linesmap44*7
-linesmap45 = [0]
-linesmap45 = linesmap45*7
-linesmap46 = [0]
-linesmap46 = linesmap46*7
-map4 = [linesmap40, linesmap41, linesmap42, linesmap43, linesmap44, linesmap45, linesmap46]
-map4[2][2] = 1
-map4[2][4] = 1
-map4[4][2] = 1
-map4[4][4] = 1
-map4[5][0] = 1
-map4[6][0] = 1
-map4[5][6] = 1
-map4[6][6] = 1
-map4[6][2] = 1
-map4[6][3] = 1
-map4[6][4] = 1
-
+def map4(map4):
+    map4[2][2] = 1
+    map4[2][4] = 1
+    map4[4][2] = 1
+    map4[4][4] = 1
+    map4[5][0] = 1
+    map4[6][0] = 1
+    map4[5][6] = 1
+    map4[6][6] = 1
+    map4[6][2] = 1
+    map4[6][3] = 1
+    map4[6][4] = 1
+    return map4
 #sixth map
-linesmap50 = [0]
-linesmap50 = linesmap50*7
-linesmap51 = [0]
-linesmap51 = linesmap51*7
-linesmap52 = [0]
-linesmap52 = linesmap52*7
-linesmap53 = [0]
-linesmap53 = linesmap53*7
-linesmap54 = [0]
-linesmap54 = linesmap54*7
-linesmap55 = [0]
-linesmap55 = linesmap55*7
-linesmap56 = [0]
-linesmap56 = linesmap56*7
-map5 = [linesmap50, linesmap51, linesmap52, linesmap53, linesmap54, linesmap55, linesmap56]
-map5[0][0] = 1
-map5[0][6] = 1
-map5[1][2] = 1
-map5[1][4] = 1
-map5[3][1] = 1
-map5[3][2] = 1
-map5[3][4] = 1
-map5[3][5] = 1
-map5[5][2] = 1
-map5[5][3] = 1
-map5[5][4] = 1
+def map5(map5):
+    map5[0][0] = 1
+    map5[0][6] = 1
+    map5[1][2] = 1
+    map5[1][4] = 1
+    map5[3][1] = 1
+    map5[3][2] = 1
+    map5[3][4] = 1
+    map5[3][5] = 1
+    map5[5][2] = 1
+    map5[5][3] = 1
+    map5[5][4] = 1
+    return map5
 
 #field to communicate with player
 linesplayer0 = ["o"]
